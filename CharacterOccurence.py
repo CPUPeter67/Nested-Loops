de15be35d776e98ad9ae69967eaa64a1e997c1c6
CPUPetet <c.nwodo@abujaprep.com>
2026-04-19 17:22:01 +0100

    Character_occurence_checker

---
+++ CharacterOccurence.py
@@ -0,0 +1,14 @@
+string = input("Please enter a word: ")
+
+char = input("Please enter a letter/character from the alphabet: ")
+
+i = 0
+count = 0
+
+while(i < len(string)): 
+
+    if (string[i] == char):
+         count = count + 1
+    i = i + 1
+
+print("The total number of occurrences of '", char, "' in '", string, "' is: ", count)
